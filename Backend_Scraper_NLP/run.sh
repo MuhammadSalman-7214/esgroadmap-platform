34cfd3257c43c273af0518b24ec10673b543f7aa
a-hamza84 <ibrahimameerhamza84@gmail.com>
2025-01-10 03:29:35 +0500

    Update run.sh

--- Backend_Scraper_NLP/run.sh
+++ Backend_Scraper_NLP/run.sh
@@ -12,7 +12,7 @@ run_scripts_in_parallel() {
     rm -f log1.log log2.log
 
     # Start a new tmux session
-    tmux new-session -d -s "parallel_scripts" \; \
+    tmux new-session -d -s "parallel_scripts2" \; \
         send-keys "python3 $script1 && echo \"$(date): $script1 completed\" && echo 'completed_flag = 1' > log1.log" C-m \; \
         split-window -h \; \
         send-keys "python3 $script2 && echo \"$(date): $script2 completed\" && echo 'completed_flag = 1' > log2.log" C-m \; \
@@ -31,10 +31,10 @@ log_message() {
 
 # Check if both scripts have completed in a loop
 while true; do
-    if tmux list-sessions | grep -q "parallel_scripts"; then
+    if tmux list-sessions | grep -q "parallel_scripts2"; then
         if [[ -f "log1.log" && -f "log2.log" ]]; then
             log_message "Both scripts have completed. Deleting session..."
-            #tmux kill-session -t "parallel_scripts"
+            #tmux kill-session -t "parallel_scripts2"
             # Change directory to Database KPI
             #cd "Database KPI"
             #log_message "1"
@@ -44,7 +44,7 @@ while true; do
             python3 "/home/ubuntu/backend_scraper_nlp/Database KPI/Unique Data Table.py"
             
             log_message "Sent KPI and Unique Data Table, Turning off VM"
-            tmux kill-session -t "parallel_scripts"
+            tmux kill-session -t "parallel_scripts2"
             break
         else
             log_message "Some scripts have not completed."
@@ -56,5 +56,7 @@ while true; do
     sleep 1
 done
 
+#kill all chrome processes
+killall -9 chrome
 # Make a curl request and log the result
 curl https://6bjiql6f3x6tgzs2ev66igseti0uzmat.lambda-url.us-east-2.on.aws/ >> "$log_file" 2>&1
