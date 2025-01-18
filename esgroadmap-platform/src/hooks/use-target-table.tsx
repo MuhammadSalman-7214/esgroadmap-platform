db61a5de7fb19f1b70944dd3216f81cc383129a1
sivasundari075 <SBalasubramaniyan@slb.com>
2025-01-18 23:02:41 +0100

    Update Country codes with Hover effects

--- esgroadmap-platform/src/hooks/use-target-table.tsx
+++ esgroadmap-platform/src/hooks/use-target-table.tsx
@@ -25,6 +25,47 @@ const representativesItemTemplate = (option: any) => {
 	);
 };
 
+// Update country code mapping with more countries
+const countryCodeMap: Record<string, string> = {
+	'United States': 'US',
+	'Australia': 'AUS',
+	'United Kingdom': 'UK',
+	'Canada': 'CAN',
+	'China': 'CN',
+	'Japan': 'JP',
+	'Germany': 'DE',
+	'France': 'FR',
+	'India': 'IN',
+	'Brazil': 'BR',
+	'South Korea': 'KR',
+	'Singapore': 'SG',
+	'Netherlands': 'NL',
+	'Switzerland': 'CH',
+	'Sweden': 'SE',
+	'Norway': 'NOR',
+	'Denmark': 'DK',
+	'Finland': 'FIN',
+	'Italy': 'IT',
+	'Spain': 'ES',
+	'Belgium': 'BE',
+	'Ireland': 'IE',
+	'New Zealand': 'NZ',
+	'Austria': 'AT',
+	'Portugal': 'PT',
+	'Luxembourg': 'LUX',
+	'Taiwan': 'TAI',
+	'Russia': 'RUS',
+	'Colombia': 'COL',
+	'Thailand': 'THAI',
+	// Add more as needed
+};
+
+// Add reverse mapping for filters
+const reverseCountryMap = Object.entries(countryCodeMap).reduce((acc, [full, code]) => {
+	acc[code] = full;
+	return acc;
+}, {} as Record<string, string>);
+
 const getFilterData = <T extends object>(data: Array<T>) => {
 	let filterKeysData: Partial<
 		Record<keyof typeof filters, Array<{ name: string }>>
@@ -33,13 +74,20 @@ const getFilterData = <T extends object>(data: Array<T>) => {
 	Object.keys(data[0]).forEach((key) => {
 		if (key in filters) {
 			let filterKey = key as keyof typeof filters;
-			// If filter match mode is IN
 			if (filters[filterKey] === FilterMatchMode.IN) {
-				let filterData = [
-					...new Set(data.map((i) => i[key as keyof T]) as string[]),
-				].map((name) => ({ name }));
+				let filterData: string[];
+				
+				// Special handling for Country column
+				if (key === dbColumns.TargetSentenceView.Country) {
+					filterData = [...new Set(data.map((i) => {
+						const country = i[key as keyof T] as string;
+						return countryCodeMap[country] || country;
+					}))];
+				} else {
+					filterData = [...new Set(data.map((i) => i[key as keyof T]) as string[])];
+				}
 
-				filterKeysData[filterKey] = filterData;
+				filterKeysData[filterKey] = filterData.map(name => ({ name }));
 			}
 		}
 	});
@@ -54,18 +102,28 @@ const getMultiSelectFilterTemplate = (templateOptions: {
 	const renderComponent: React.ComponentProps<
 		typeof Column
 	>["filterElement"] = (options) => {
+		// Special handling for Country filter
+		const isCountryFilter = templateOptions.key === dbColumns.TargetSentenceView.Country;
 
 		return (
 			<MultiSelect
 				value={
 					options.value === null
 						? []
-						: options.value.map((i: any) => ({ name: i }))
+						: options.value.map((i: string) => ({ 
+							name: isCountryFilter ? (countryCodeMap[i] || i) : i 
+						}))
 				}
 				options={templateOptions.data}
 				itemTemplate={representativesItemTemplate}
 				onChange={(e) => {
-					options.filterApplyCallback(e.value.map((i: any) => i.name));
+					const values = e.value.map((i: any) => {
+						if (isCountryFilter) {
+							return reverseCountryMap[i.name] || i.name;
+						}
+						return i.name;
+					});
+					options.filterApplyCallback(values);
 				}}
 				optionLabel="name"
 				filter
@@ -139,6 +197,34 @@ const useTargetTable = <T extends object>(data: Array<T>) => {
 
 			if (value === null) {
 				return "N/A";
+			}
+
+			// Updated country code display with tooltip
+			if (key === dbColumns.TargetSentenceView.Country) {
+				return (
+					<div className="relative group">
+						<span>{countryCodeMap[value] || value}</span>
+						<div className="
+							absolute 
+							hidden 
+							group-hover:block 
+							bg-gray-800 
+							text-white 
+							text-sm 
+							px-2 
+							py-1 
+							rounded 
+							-top-8 
+							left-1/2 
+							transform 
+							-translate-x-1/2 
+							whitespace-nowrap
+							z-50
+						">
+							{value}
+						</div>
+					</div>
+				);
 			}
 
 			if (key === dbColumns.TargetSentenceView.DocURL) {
