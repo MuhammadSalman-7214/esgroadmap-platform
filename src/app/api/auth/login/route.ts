8eb5b80ec671ac93b68cb0679902364e90877839
Muhammad Salman <m.salman7214@gmail.com>
2025-03-05 22:40:44 +0500

    config added

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,35 +1,34 @@
-import { HttpForbiddenError, HttpNotFoundError } from '@/errors'
-import prisma from '@/lib/prisma'
-import authValidator, { LoginSchema } from '@/server/validators/auth.validator'
-import handleError from '@/utils/handle-error'
-import token from '@/utils/token'
-import validate from '@/utils/validate'
-import { HttpStatusCode } from 'axios'
-import { compare } from 'bcrypt'
-import { NextResponse } from 'next/server'
-
+import {HttpForbiddenError, HttpNotFoundError} from '@/errors';
+import prisma from '@/lib/prisma';
+import authValidator, {LoginSchema} from '@/server/validators/auth.validator';
+import handleError from '@/utils/handle-error';
+import token from '@/utils/token';
+import validate from '@/utils/validate';
+import {HttpStatusCode} from 'axios';
+import {compare} from 'bcrypt';
+import {NextResponse} from 'next/server';
+export const config = {
+  runtime: 'nodejs',
+};
 export async function POST(request: Request) {
   try {
-    const json = await request.json()
-    const body = await validate<LoginSchema>(authValidator.login, json)
+    const json = await request.json();
+    const body = await validate<LoginSchema>(authValidator.login, json);
 
     const foundUser = await prisma.user.findFirst({
       where: {
-        OR: [
-          { email: body.usernameOrEmail },
-          { username: body.usernameOrEmail },
-        ],
+        OR: [{email: body.usernameOrEmail}, {username: body.usernameOrEmail}],
       },
-    })
+    });
 
     if (!foundUser) {
-      throw new HttpForbiddenError('Invalid credentials')
+      throw new HttpForbiddenError('Invalid credentials');
     }
 
-    const isValid = await compare(body.password, foundUser.password)
+    const isValid = await compare(body.password, foundUser.password);
 
     if (!isValid) {
-      throw new HttpForbiddenError('Invalid credentials')
+      throw new HttpForbiddenError('Invalid credentials');
     }
 
     const payload = {
@@ -37,9 +36,9 @@ export async function POST(request: Request) {
       email: foundUser.email,
       username: foundUser.username,
       role: foundUser.role,
-    }
+    };
 
-    const access = token.access.sign(payload)
+    const access = token.access.sign(payload);
 
     return NextResponse.json(
       {
@@ -56,9 +55,9 @@ export async function POST(request: Request) {
         headers: {
           'Set-Cookie': `token=${access.token};Max-Age=${access.maxAge}; Path=/; SameSite=None; Secure`,
         },
-      },
-    )
+      }
+    );
   } catch (error) {
-    return handleError(error)
+    return handleError(error);
   }
 }
